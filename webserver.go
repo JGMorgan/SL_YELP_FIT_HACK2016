da101bacbe1fe55d953f6bd2ec714420fcd3bb3e
JGMorgan <josegm11194@gmail.com>
2016-04-02 13:48:45 -0400

    starting over with our own chat

---
+++ webserver.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var connections map[*websocket.Conn]bool
+
+func main() {
+	port := flag.Int("port", 8000, "")
+	dir := flag.String("indexLocation", "", "")
+
+	flag.Parse()
+	connections = make(map[*websocket.Conn]bool)
+
+	fs := http.Dir(*dir)
+	fileHandler := http.FileServer(fs)
+	http.Handle("/", fileHandler)
+
+	http.HandleFunc("/sock", handler)
+	log.Printf("FTHacks server started on port %d\n", *port)
+
+	addr := fmt.Sprintf("localhost:%d", *port)
+	err := http.ListenAndServe(addr, nil)
+	fmt.Println(err.Error())
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	_, ok := err.(websocket.HandshakeError)
+	if ok {
+		http.Error(w, "error", 400)
+		return
+	} else if err != nil {
+		log.Println(err)
+		return
+	}
+
+	connections[conn] = true
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			delete(connections, conn)
+			conn.Close()
+			return
+		}
+		sendAll(msg)
+	}
+}
+
+func sendAll(message []byte) {
+	for conn := range connections {
+		err := conn.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			delete(connections, conn)
+			conn.Close()
+		}
+	}
+}
