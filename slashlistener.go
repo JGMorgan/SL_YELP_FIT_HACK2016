a004253852b2e1c7e7f3aea87468e4b3fd947c3a
JGMorgan <josegm11194@gmail.com>
2016-04-02 12:32:46 -0400

    maybe it works

--- slashlistener.go
+++ slashlistener.go
@@ -1,1 +1,27 @@
 package slashlistener
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+//handles http request from slack
+func handler(w http.ResponseWriter, r *http.Request) {
+	cmd := r.FormValue("command")
+
+	if cmd == "/yelp" {
+		_, err := fmt.Fprintln(w, "SWAG")
+		if err != nil {
+			fmt.Println(":( it broke, call bestbuy")
+		}
+	} else {
+		_, err := fmt.Fprintln(w, "u wut m8?")
+		if err != nil {
+			fmt.Println("Plz send help bestbuy")
+		}
+	}
+}
